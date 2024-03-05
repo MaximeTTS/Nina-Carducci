1c3ed12f262a25d00a25d9f1cdbe0be27aafeeb5
MaximeTTS <maxime.s.turquet@gmail.com>
2024-03-05 16:53:41 +0100

    -Column footer for phone format -Adjust header for About -Adjustment of H1, more legible and text in center with padding adjustment on top. -Addition of slider photo size -Adjusted font size for header and block note

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -164,7 +164,7 @@
                     <div class="modal-content">
                         <div class="modal-body">
                             ${navigation ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>' : '<span style="display:none;" />'}
-                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
+                            <img class="lightboxImage img-fluid" alt="Con+tenu de l'image affichée dans la modale au clique"/>
                             ${navigation ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>' : '<span style="display:none;" />'}
                         </div>
                     </div>
